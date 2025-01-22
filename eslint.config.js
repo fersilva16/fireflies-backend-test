c323350aa5cd41b030ce82a2ed4aa427ef43afed
Fernando Silva <fernandonsilva16@gmail.com>
2025-01-22 10:19:12 -0300

    refactor(eslint): add and lint (#4)

---
+++ eslint.config.js
@@ -0,0 +1,74 @@
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+import eslint from 'typescript-eslint';
+
+export default eslint.config({
+  extends: eslint.configs.strictTypeChecked,
+  ignores: ['**/dist/**/*.js'],
+  plugins: {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    import: eslintPluginImport,
+     
+    'unused-imports': eslintPluginUnusedImports,
+  },
+  rules: {
+    '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+    '@typescript-eslint/restrict-template-expressions': [
+      'error',
+      {
+        allowAny: false,
+        allowBoolean: false,
+        allowNullish: false,
+        allowNumber: true,
+        allowRegExp: false,
+      },
+    ],
+    '@typescript-eslint/no-unnecessary-condition': [
+      'error',
+      {
+        allowConstantLoopConditions: true,
+      },
+    ],
+    'no-console': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling', 'index'],
+        ],
+        distinctGroup: false,
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+        pathGroupsExcludedImportTypes: ['internal'],
+      },
+    ],
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+  languageOptions: {
+    parserOptions: {
+      tsconfigRootDir: import.meta.dirname,
+      project: './tsconfig.eslint.json',
+    },
+  },
+});
