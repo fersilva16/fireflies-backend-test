22389630d66a5c160ac6a6eb606ce8098fa444df
Fernando Silva <fernandonsilva16@gmail.com>
2025-01-22 10:03:22 -0300

    feat(nix): add flake and direnv

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Fireflies.ai Backend Test";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            nodejs
+          ];
+        };
+      }
+    );
+}
