3f522037a3b465068c3ffa00317a1f0712d4b1c5
Fernando Silva <fernandonsilva16@gmail.com>
2025-01-22 15:28:35 -0300

    ci(test): add test with coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,14 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     setupFiles: ['./test/setup.ts'],
+    coverage: {
+      include: ['src/**/*.ts'],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
+    },
   },
 });
